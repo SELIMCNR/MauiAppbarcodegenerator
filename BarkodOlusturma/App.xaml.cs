2b624bcc3877a690bbb4e5be830996d9f15ae9b9
Selim Çınar <btk02672@gmail.com>
2024-12-17 16:32:13 +0300

    first commit

---
+++ BarkodOlusturma/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BarkodOlusturma
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BarkodOlusturma/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BarkodOlusturma
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
